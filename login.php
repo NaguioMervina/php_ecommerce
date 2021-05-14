367597ea53607101d368c4574cc2e4c91d527d11
Mervin Naguio <78600968+NaguioMervina@users.noreply.github.com>
2021-05-14 11:28:54 +0800

    login and register integration

---
+++ login.php
@@ -0,0 +1,19 @@
+<div class="login-form">
+
+<a href="javascript:void(0);" class="form-cancel">
+    <i class='bx bx-x'></i>
+</a>
+
+<strong>Log In</strong>
+<img src="shop/images/logo/1.png" alt="">
+
+<form>
+    <input type="email" placeholder="@gmail.com" name="email" required>
+    <input type="password" placeholder="Password.." name="password" required>
+    <input type="submit" value="Log In">
+</form>
+<div class="form-btn">
+    <a href="#" class="forget">Forget Your Password?</a>
+    <a href="javascript:void(0);" class="sign-up">Create Account</a>
+</div>  
+</div>
--- top.php
+++ top.php
@@ -72,47 +72,16 @@
 
             <div class="form">
 
-                <div class="login-form">
 
-                    <a href="javascript:void(0);" class="form-cancel">
-                        <i class='bx bx-x'></i>
-                    </a>
-                    
-                    <strong>Log In</strong>
-                    <img src="shop/images/logo/1.png" alt="">
 
-                    <form>
-                        <input type="email" placeholder="@gmail.com" name="email" required>
-                        <input type="password" placeholder="Password.." name="password" required>
-                        <input type="submit" value="Log In">
-                    </form>
-                    <div class="form-btn">
-                        <a href="#" class="forget">Forget Your Password?</a>
-                        <a href="javascript:void(0);" class="sign-up">Create Account</a>
-                    </div>  
-                </div>
+            <?php require('login.php')?>
+
+
+
+            <?php require('register.php')?>
+               
 
 
 
 
-                <div class="signup-form">
-
-                    <a href="javascript:void(0);" class="form-cancel">
-                        <i class='bx bx-x'></i>
-                    </a>
-                    
-                    <strong>Sign Up</strong>
-                    <img src="shop/images/logo/1.png" alt="">
-
-                    <form>
-                        <input type="text" placeholder="Full Name" name="fullname" required>
-                        <input type="email" placeholder="@gmail.com" name="email" required>
-                        <input type="password" placeholder="Password.." name="password"required>
-                        <input type="submit" value="Sign up">
-                    </form>
-                    <div class="form-btn">
-                        <a href="javascript:void(0);" class="already-acc">Already Have Account?</a>
-                   
-                    </div>  
-                </div>
             </div>
